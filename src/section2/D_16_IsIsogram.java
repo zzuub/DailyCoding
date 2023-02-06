35c7058f0f26d7bd783705a5b8eb67d1cb7add42
Reese <ajw5921@gmail.com>
2023-02-06 09:15:49 +0900

    Day 16

--- src/section2/D_16_IsIsogram.java
+++ src/section2/D_16_IsIsogram.java
@@ -7,14 +7,14 @@ public class D_16_IsIsogram {
         if(str.length() == 0) {
             return true;
         }
-        HashMap<Character, String> cache = new HashMap<Character, String>();
+        HashMap<Character, String> hm = new HashMap<Character, String>();
         str = str.toLowerCase();
 
         for(int i = 0; i < str.length(); i++) {
-            if(cache.containsKey(str.charAt(i))) {
+            if(hm.containsKey(str.charAt(i))) {
                 return false;
             }
-            cache.put(str.charAt(i), "Exists");
+            hm.put(str.charAt(i), "Exists");
         }
         return true;
     }
