660afa5da60769c1fdae9872d8935b243cb4bb30
Reese <ajw5921@gmail.com>
2023-02-15 09:50:36 +0900

    Day 22

--- src/section2/D_22_BubbleSort.java
+++ src/section2/D_22_BubbleSort.java
@@ -2,14 +2,6 @@ package section2;
 
 public class D_22_BubbleSort {
     public int[] swap(int idx1, int idx2, int[] arr) {
-        int temp = arr[idx1];
-        arr[idx1] = arr[idx2];
-        arr[idx2] = temp;
-
-        return arr;
-    }
-
-    public int[] bubbleSort(int[] arr) {
         int arrLength = arr.length;
 
         for(int i = 0; i < arrLength; i++) {
